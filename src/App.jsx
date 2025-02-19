9a03c30416d8ad634c97b453c454dfb981a1273c
Prince-Sharma002 <prince12845sharma@gmail.com>
2025-02-19 14:35:50 +0530

    deleting files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import { Button } from "./components/ui/button.jsx";
-
-import { CalendarIcon, HomeIcon, MailIcon, PencilIcon } from "lucide-react";
 import React from "react";
  
-import { buttonVariants } from "./components/ui/button";
-import { cn } from "./lib/utils.js";
-import { Dock, DockIcon } from "./components/magicui/dock.jsx";
- 
-import Navbar from "./components/Navbar.jsx";
-import HeroSection from "./components/HeroSection.jsx";
-import AutoPlayMusic from "./components/AutoPlayMusic.jsx";
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <Navbar /> */}
-    <HeroSection />
-    {/* <AutoPlayMusic /> */}
+      <h1>app</h1>
     
 
     </>
