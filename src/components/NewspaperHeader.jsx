dd0f615fb2d66e99255e82a1a645915a5ed73eec
Amit Verma <vamit20000@gmail.com>
2025-02-20 00:31:03 +0530

    navbar update

--- src/components/NewspaperHeader.jsx
+++ src/components/NewspaperHeader.jsx
@@ -1,52 +1,76 @@
 import hacknovate from "../assets/hacknovate.png";
-
 const NewspaperHeader = () => {
   return (
     <header className="w-full px-0 py-6 bg-prophet-gray bg-opacity-20">
       <div className="w-full mx-auto border-b-4 border-slate-800">
-        <div className="flex items-center justify-start mb-4 animate-fadeIn">
-          <div className="flex items-center space-x-2">
-            <div className="w-20 h-20">
-              <div className="text-center leading-tight">
-                <img src={hacknovate} className="w-[160px] h-[110px]" alt="logo" />
-              </div>
-            </div>
-          </div>
-        </div>
-
         <div
-          className="text-center space-y-4 animate-fadeIn"
+          className="text-center space-y-3 animate-fadeIn"
           style={{ animationDelay: "0.2s" }}
         >
-          <div className="flex items-center justify-center space-x-4">
-            <div className="flex-1 h-px bg-prophet-black border-y-2 border-slate-800"></div>
-            <span>
-              <img src={hacknovate} width={20} height={20} alt="logo" />
+          <div
+            id="navbar"
+            className="flex items-center justify-center space-x-6 max-lg:hidden "
+          >
+            <div className="flex-1  border-y-2 border-slate-800"></div>
+            <span className="font-serif font-bold hover:text-amber-500 text-xl">
+              <a href="">About</a>
             </span>
-            <span className="font-crimson text-s md:text-xl font-serif">
-              FLAGSHIP EVENT
+            <span className="font-serif font-bold hover:text-amber-500 text-xl">
+              <a href="">Events</a>
+            </span>
+            <span className="font-serif font-bold hover:text-amber-500 text-xl">
+              <a href="">Team</a>
+            </span>
+            <span className="font-serif font-bold hover:text-amber-500 text-xl">
+              <a href="">Mentor</a>
+            </span>
+            <span className="font-serif font-bold hover:text-amber-500 text-xl">
+              <a href="">Contact Us</a>
             </span>
 
             <div className="flex-1 h-px bg-prophet-black border-y-2 border-slate-800"></div>
-            {/* <span className="text-prophet-black italic font-crimson text-sm">
-              spellbindingly orchestrated chronicle
-            </span> */}
+            <button className="border-2 text-center rounded-full font-bold font-serif p-1 ">
+              {" "}
+              Sponsor US{" "}
+            </button>
+          </div>
+          <div className=" h-16 w-full border-2 border-red-500 hidden max-lg:block">
+            dasdad
+          </div>
+          <div className="flex justify-between max-lg:justify-center items-center mb-0">
+            <img
+              src={hacknovate}
+              alt=""
+              height={100}
+              width={120}
+              className="relative left-28  max-lg:hidden"
+            />
+            <h1 className="text-2xl md:text-8xl font-serif font-black tracking-wide  ">
+              <span className="text-prophet-black tracking-wide max-lg:text-4xl">
+                ABESIT{" "}
+              </span>
+              <span className="text-prophet-gold text-amber-400 tracking-wide max-lg:text-4xl ">
+                T
+              </span>
+              <span className="text-prophet-black tracking-wide max-lg:text-4xl">
+                IMES
+              </span>
+            </h1>
+            <img
+              src={hacknovate}
+              alt=""
+              height={100}
+              width={120}
+              className="relative right-28 max-lg:hidden"
+            />
           </div>
 
-          <h1 className="text-2xl md:text-8xl font-playfair font-black tracking-wider">
-            <span className="text-prophet-black tracking-tight">ABESIT </span>
-            <span className="text-prophet-gold text-amber-400 tracking-tight ">
-              T
-            </span>
-            <span className="text-prophet-black tracking-tight">IMES</span>
-          </h1>
-
-          <div className="flex items-center justify-center space-x-4">
-            <span className="text-prophet-black text-2xl">★</span>
-            <p className="font-crimson text-prophet-black uppercase tracking-widest text-sm md:text-base text-center ">
-              The Wizard World Beguiling Broadsheet of Choice
+          <div className="flex items-center justify-center space-x-4 mt-0">
+            <span className=" text-2xl mb-1 pb-6 max-lg:text-xs">★</span>
+            <p className="font-crimson text-xl font-serif uppercase tracking-widest pb-6 md:text-base text-center mb-1 max-lg:text-xs ">
+              Presents their FLAGSHIP EVENT
             </p>
-            <span className="text-prophet-black text-2xl">★</span>
+            <span className=" text-2xl mb-1 pb-6 max-lg:text-xs">★</span>
           </div>
         </div>
       </div>
