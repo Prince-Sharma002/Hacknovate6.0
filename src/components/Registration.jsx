0b9367e0628055e15dd31639d220ced19e45c92e
Prince-Sharma002 <prince12845sharma@gmail.com>
2025-02-20 12:04:21 +0530

    sponsor added

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import discord from "../assets/discord.png"
+import { FaDiscord } from "react-icons/fa";
+import { SiLinktree } from "react-icons/si";
 
 const Registration = () => {
   return (
@@ -8,12 +11,20 @@ const Registration = () => {
             <p className='p-2 px-2 text-4xl bg-black text-white w-full h-full text-center flex justify-center align-center items-center'> Register Now </p>
         </div>
         <div>
-            <p className='text-justify justify-last w-full h-full text-center flex justify-center align-center items-center'> Discord and other button </p>
+            <p className='text-justify justify-last w-full h-full text-center flex justify-center align-center items-center pl-12'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequuntur culpa quia facilis? </p>
         </div>
-        <div>
-            <p className='text-justify justify-last w-full h-full text-center flex justify-center align-center items-center'> Discord and other button </p>
+        <div className='flex flex-row justify-center align-center items-center'>
+            <div className='flex flex-col justify-center align-center items-center'>
+                <FaDiscord className="text-8xl" />
+                <button> DISCORD </button>
+            </div>
+            <div className='flex flex-col justify-center align-center items-center'>
+                <FaDiscord className="text-8xl" />
+                <button> DISCORD </button>
+            </div>
+
         </div>
-        <div className='flex flex-between'>
+        <div className='flex flex-between justify-center align-center items-center'>
 
                 <div className='grow'>
                     <h1 className='text-4xl'> DATE </h1>
