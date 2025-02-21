13231e399b38148da2d2db637fd106e48f485e9e
Amit Verma <vamit20000@gmail.com>
2025-02-21 16:24:15 +0530

    prize section

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Registration from "./components/Registration.jsx";
 import NewspaperHeader from "./components/NewspaperHeader";
 import About from "./components/About.jsx";
 import Spnosor from "./components/Spnosor.jsx";
+import Prizes from "./components/Prizes.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Spnosor />
       <Registration />
       <About />
+      <Prizes/>
    
     </>
   );
