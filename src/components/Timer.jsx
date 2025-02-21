08a137c4435e481256f80eb6b3ac58fb24bbd7b6
Aayush Shukla <aayushshukla1438@gmial.com>
2025-02-21 22:13:44 +0530

    change

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -14,6 +14,7 @@ const Timer = ({ targetDate }) => {
     }
     return { days: 0, hours: 0, minutes: 0, seconds: 0 };
   };
+  
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
 
