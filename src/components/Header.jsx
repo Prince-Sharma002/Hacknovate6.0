56e38a2e64e559c512f6ae5460f34734ea0c909d
Prince-Sharma002 <prince12845sharma@gmail.com>
2025-02-27 13:49:53 +0530

    sponsor text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,17 +9,17 @@ const Header = () => {
       {/* Left Column - Grey Box */}
       <div className="flex max-lg:relative justify-center md:justify-start max-lg:-top-8">
         <div className="w-[400px] md:w-[507px] h-[406px] rounded-[20px]  bg-[#C9C8C8] grid grid-rows-3 gap-4 p-4">
-               <div className="flex gap-12 ">
+               {/* <div className="flex gap-12 ">
                   <div className="w-32 bg-gray-600"> </div>
                   <div> 
                     <h1 className="text-8xl text-left">ABC</h1>
                     <h1>PRESENTS</h1>
                    </div>
-               </div>
+               </div> */}
 
                <h1 className="text-center text-6xl font-extrabold"> <span className="border-b-4"> Hacknovate 6.0 </span> </h1>
 
-               <div className="grid grid-cols-2">
+               {/* <div className="grid grid-cols-2">
                   <div> 
                       <h1>XYZ</h1>
                       <h1>Powered By-</h1>
@@ -30,7 +30,7 @@ const Header = () => {
                       <h1>Organized by</h1>
                   </div>
                   </div>
-               </div>
+               </div> */}
 
         </div>
       </div>
