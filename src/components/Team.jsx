d7441599bb23dc3d694df87776c6c20d52191542
Utkarsh Aggarwal <utkarshaggarwalcse@gmail.com>
2025-02-27 14:57:36 +0530

    added more containne solved resp0nsiveness

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -34,6 +34,16 @@ const Team = () => {
                 <h4>The "Remus Lupin and Ginny Weasley" duoRemus brings thoughtful, strategic designs as sharp as a werewolf’s instincts, while Ginny adds flair and precision, making everything as vibrant as a Quidditch match.</h4>
               </div>
             </div>
+            <div className="flex flex-col md:flex-row gap-4">
+              <div className="w-full md:w-1/2 text-center text-gray-700 font-bold">
+                <img src={blank2} alt="Nikita tech lead" className="w-full h-auto" />
+                <h4>Meet our Hermione—the Tech & Design Sorceress of Hacknovate 6.0! As Tech Lead, Social Media Lead, and Design Contributor, she crafts an experience as flawless as Wingardium Leviosa, ensuring every detail is pure magic!.</h4>
+              </div>
+              <div className="w-full md:w-1/2 text-center text-gray-700 font-bold">
+                <img src={Prithvi} alt="design leads " className="w-full h-[300px] object-cover" />
+                <h4>The "Remus Lupin and Ginny Weasley" duoRemus brings thoughtful, strategic designs as sharp as a werewolf’s instincts, while Ginny adds flair and precision, making everything as vibrant as a Quidditch match.</h4>
+              </div>
+            </div>
             <div className="w-full text-center text-gray-700 font-bold">
               <img src={parth} alt="Tech & Design Sorceress" className="w-full h-auto" />
               <h4 className="text-justify leading-6">Bringing wisdom, creativity, and skill to Hacknovate 6.0, this trio ensures a flawless tech experience. *Lovegood* adds imagination and fresh ideas, making innovation feel effortless. *Weasley, with his cleverness and quick wit, keeps things running smoothly with sharp fixes and fun. **Tonks*, ever-adaptable, tackles challenges with rebellious brilliance. Together, they make tech feel like magic! .</h4>
@@ -52,7 +62,12 @@ const Team = () => {
               <h4>Every great hackathon needs a sharp mind with an edge, and *Malfoy* brings his signature mastery to the technical team of Hacknovate 6.0. With the cunning of a true strategist and the precision of a perfectly brewed potion, he ensures the tech runs flawlessly—because in his world, excellence isn’t a choice, it’s a legacy...</h4>
             </div>
             <div className="w-full text-center text-gray-700 font-bold">
-              <img src={gunabh} alt="Content Lea" className="w-full h-auto" />
+              <img src={pratyush} alt="Discord Admin" className="w-full h-auto" />
+              <h4>Like Remus Lupin guiding young wizards, he built the Hacknovate 6.0 Discord Realm, the backbone of our online hackathon. As the Master of the Floo Network, he ensures smooth communication, seamless collaboration, and an enchanting experience for all. His wizardry keeps our digital Hogwarts alive!</h4>
+            </div>
+            
+            <div className="w-full text-center text-gray-700 font-bold">
+              <img src={gunabh} alt="Content Lead" className="w-full h-auto" />
               <h4>The "Luna Lovegood and Rubeus Hagrid" duoLuna brings the whimsical creativity, dreaming up ideas as unique as a Crumple-Horned Snorkack, while Hagrid adds heart and passion, crafting content as warm and magical as a Care of Magical Creatures lesson...</h4>
             </div>
           </div>
