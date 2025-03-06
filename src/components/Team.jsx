c2559651a827ef1ddb7f7167223b95dfd5898740
VishalTechnohunk <118457719+VishalTechnohunk@users.noreply.github.com>
2025-03-06 10:30:48 +0530

    team changes styling

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -111,7 +111,7 @@ const Team = () => {
 
                 
 
-              <div className="team-card md:h-[40rem] md:mt-32">
+              <div className="team-card md:h-[40rem] mt-64">
                   <div className="relative group">
                     <img src={mathur} alt="Malfoy" className="team-image" />
                     <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white">
@@ -125,9 +125,28 @@ const Team = () => {
                     </div>
                   </div>
                   <div className="team-description md:h-[100%]">
-                    <p>Like James Potter guiding young wizards, he built the Hacknovate 6.0 Discord Realm, the backbone of our online hackathon. As the Master of the Floo Network, he ensures smooth communication, seamless collaboration, and an enchanting experience for all. His wizardry keeps our digital Hogwarts alive!</p>
+                 <p>Like James Potter guiding young wizards,  he crafted the Hacknovate 6.0 Discord Realm, the hackathon’s backbone. As Master of the Floo Network, he ensures seamless communication and collaboration. His expertise creates a digital Hogwarts where innovation thrives, ideas soar, making Hacknovate 6.0 a truly spellbinding adventure!
+                 </p>
                   </div>
                 </div>
+                {/* <div className="team-card">
+                  <div className="relative group">
+                    <img src={raunak} alt="Social Media Lead" className="team-image" />
+                    <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex flex-col justify-center items-center text-white">
+                      <div className="flex flex-col items-center">
+                        <span className="text-xl font-bold">Social Lead</span>
+                        <span className="text-sm mt-2">Raunak</span>
+                        <a href="https://www.linkedin.com/in/raunak-singh-2b747518a/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-400 transition-colors mt-3">
+                          <FaLinkedin className="w-6 h-6" />
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="team-description">
+                    <p>Meet Skeeter—our Social Media Lead and the (way more ethical) Rita Skeeter of
+                    Hacknovate! With a Quick-Quotes Quill and a sharp strategy, he spreads the magic far and wide!</p>
+                  </div>
+                </div> */}
                
                 
           
@@ -174,7 +193,7 @@ const Team = () => {
                   </div>
                   <div className="team-description">
                     <p>
-                      Meet our Hermione—the Tech & Design Sorceress of Hacknovate 6.0! As Tech Lead, Social Media Lead, and Design Contributor, she crafts an experience as flawless as Wingardium Leviosa, ensuring every detail is pure magic!  
+                    Meet our Hermione—the Tech & Design Sorceress of Hacknovate 6.0! With her exceptional skills and boundless creativity, she works her magic as the Tech Lead, Social Media Lead, and Design Contributor, weaving together an experience as seamless as casting Wingardium Leviosa. From coding spells to enchanting visuals and engaging content, she ensures every aspect of the event is nothing short of extraordinary. 
                   </p>
                   </div>
                 </div>
@@ -208,8 +227,8 @@ const Team = () => {
                 <p>Like the Gringotts wizards guarding the vaults, this powerhouse team ensures Hacknovate 6.0
                 is backed by the strongest alliances. With the negotiation skills of Fred & George Weasley,
                 the strategy of Percy Weasley, and the charm of a well-cast Confundus Charm, they bring in
-                the best sponsors and partnerships. Their magic keeps the event grand, ensuring every
-                galleon counts!</p>
+                the best sponsors and partnerships. Their magic keeps the event grand, ensuring every galleon counts!</p>
+                
               </div>
             </div>
           </div>
